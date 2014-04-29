f076f798a915b5897a274dfde66c6b4319c87b70
Keagan McClelland <kmcclel2@illinois.edu>
2014-04-29 16:55:16 -0500

    wrote prototype code for all uploads, awaiting bambi's response for working malware link to test

--- 460Project.py
+++ 460Project.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Feb 07 12:09:19 2014
 
-@author: Kris
+@author: kocinsk2, kmcclel2
 """
 
 import imaplib
@@ -26,6 +26,7 @@ charset, data = mail.uid('search', None, "ALL")
 recentUnreadID = data[0].split()[-1]
 charset, data = mail.uid('fetch', recentUnreadID, '(RFC822)')
 raw_mail = data[0][1]
+print raw_mail
 
 #Regex to parse out URLs
 x = re.compile(r"http://(\w*[.])*(\w*/)*(\w*[-]\w*)*[?](\w*[-]\w*)*")
@@ -41,6 +42,7 @@ filename = params['filename']
 #Download the file
 print "Beginning File Download\n"
 data = zfile.read()
+print type(data)
 with open(filename, "wb") as code:
     code.write(data)
 print "Download Complete\n"
@@ -50,9 +52,28 @@ with zipfile.ZipFile(filename, "r") as z:
     z.extractall()
 
 #Upload the file to vxcage
+# UNTESTED CODE CORRECT IN THEORY
+import os
+pathname = '/malware/'+sort+'/'+md5(fopen(filename))
+scpquery = 'scp ' + filename + ' credentials@vxcage.internetcrimefighter.org:' + pathname
+os.system(scpquery)
 
 #Upload the file to virus total
+import postfile
+host = "www.virustotal.com"
+selector = "https://www.virustotal.com/vtapi/v2/file/scan"
+fields = [("apikey", "")]
+file_to_send = open(filename, "rb").read()
+files = [("file", filename, file_to_send)]
+json = postfile.post_multipart(host, selector, fields, files)
 
 #Upload the file to totalhash
+import ftplib
+session = ftplib.FTP('ftp://totalhash.com','upload','totalhash')
+f = open(filename,'rb')                  # file to send
+session.storbinary(filename, f)     # send the file
+f.close()                                    # close file and FTP
+session.quit()
+
 
 
